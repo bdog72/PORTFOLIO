11f418fb557618621ee902a68f091f4ccd526883
Brian Bycynski <high5bri@hotmail.com>
2017-04-02 06:19:06 -0400

    changed header font size

--- src/components/App.js
+++ src/components/App.js
@@ -27,7 +27,7 @@ class App extends Component {
   render () {
     return <div>
       <header>
-        <h1>Hi, I'm {this.state.name}</h1>
+        <h1>{this.state.name}</h1>
         <Image className='pro1' src={this.state.imageURL} responsive />
         <div>
           <p>{this.state.bio}</p>
